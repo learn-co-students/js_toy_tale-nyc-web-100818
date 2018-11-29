9c2411da6e7b0c8b5010b011568e49b48f642add
yli0607x <github email address>
2018-11-29 15:23:08 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
 const toyContainer = document.querySelector('#toy-collection')
+const searchBox = document.querySelector('#search-box')
 let addToy = false
 
 // YOUR CODE HERE
@@ -17,7 +18,6 @@ addBtn.addEventListener('click', () => {
     toyForm.style.display = 'none'
   }
 })
-
 
 // OR HERE!
 //----------fetch---------//
@@ -55,31 +55,68 @@ addBtn.addEventListener('click', () => {
     e.target.reset()
   })//end of submit EventListener
 
-//-------------like button EventListener---------//
+//-------------like & delete button EventListener---------//
   toyContainer.addEventListener('click', e => {
     if (e.target.className === "like-btn"){
+      // debugger
       const likesPTag = event.target.parentElement.querySelector('p')
       const likes = parseInt(likesPTag.innerText)
       const newLikes = likes + 1
       likesPTag.innerHTML = `${newLikes} Likes`
       //end of updating DOM
       const likeId = event.target.parentElement.id
-      console.log(likeId)
+      console.log(likeId, newLikes)
       fetch(`http://localhost:3000/toys/${likeId}`, {
         method: "PATCH",
-        header: {
+        headers: {
           "Content-Type": "application/json",
-           Accept: "application/json"
+           "Accept": "application/json"
         },//end of header
         body: JSON.stringify({
-          "likes": newLikes
+           'likes': newLikes
         })//end of body
       })//end of fetch---> updating Server
       .then(r => r.json())
-      .then(updatedToy)
+      .then((data)=>console.log(data))
     }//end of if click like
 
+//------------------delete----------------//
+    else if (e.target.className ==='delete-btn') {
+      console.log(e.target);
+      const toyCard = e.target.parentElement
+      const toyCardId = toyCard.id
+      console.log(toyCardId)
+      pessimisticallyDeleteToy(toyCardId,toyCard)
+    }//
+
   })//end of like click EventListener
+
+  //------------pessimistically Delete---------------//
+    const pessimisticallyDeleteToy = (id, toyCard) => {
+      fetch(`http://localhost:3000/toys/${id}`, {method: 'DELETE'})//delete from database
+        .then(r => {
+          if(r.ok) {//if successfully delete from server
+            toyContainer.removeChild(toyCard)//then delete from DOM
+          }//end of if succesfully delete
+        })//end of then
+    }//end of delete
+
+  //-----------search----------------------//
+  searchBox.addEventListener('input', e =>{
+    const userInput = e.target.value
+    fetch('http://localhost:3000/toys')
+      .then(res => res.json())
+      .then(json => {
+        const filteredToys = json.filter((toy) => {
+          return toy.name.toLowerCase().includes(userInput) // filtered the array in memory
+        })
+        toyContainer.innerHTML = ''//clear the conatiner
+        filteredToys.forEach((toy) =>
+          toyContainer.innerHTML += renderSingleToy(toy)
+        )//end of reder filterToy
+      })//end of .teh
+  })//end of input search
+
 
 })//end of DOM Content
 
@@ -91,6 +128,7 @@ addBtn.addEventListener('click', () => {
       <img src=${toy.image} class="toy-avatar" />
       <p>${toy.likes} Likes </p>
       <button class="like-btn">Like <3</button>
+      <button class="delete-btn">Delete T^T </button>
       </div>
   `
   }//end of render single toy helper
